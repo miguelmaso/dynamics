2ed46b97997770c9356d33133a30a59178d12fc6
Miguel Masó <miguel.maso@upc.edu>
2025-12-16 14:30:03 +0100

    move to folder

--- .github/workflows/build_pdf.sh
+++ .github/workflows/build_pdf.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 ARGS="--shell-escape --file-line-error --synctex=1 --interaction=nonstopmode"
+mkdir img/figures/
 pdflatex $ARGS problems
 biber          problems
 pdflatex $ARGS problems
